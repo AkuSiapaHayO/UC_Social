e441504e57c0953cdd851c5ccd9141973df742ac
AsukaaNao <christiantanjung27@gmail.com>
2024-06-03 20:21:33 +0700

    fixed error

--- app/src/main/java/com/example/ucsocial/Routes.kt
+++ app/src/main/java/com/example/ucsocial/Routes.kt
@@ -1,20 +1,14 @@
 package com.example.ucsocial
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -23,6 +17,7 @@ import com.example.ucsocial.service.MyContainer
 import com.example.ucsocial.view.Profile
 import com.example.ucsocial.view.RegisterView
 import com.example.vp_alpapp.viewmodel.CreateContentViewModel
+import com.example.vp_alpapp.viewmodel.ExploreViewModel
 import com.example.vp_alpapp.viewmodel.HomeUIState
 import com.example.vp_alpapp.viewmodel.HomeViewModel
 import com.example.vp_alpapp.viewmodel.LoginViewModel
@@ -98,15 +93,14 @@ fun Routes() {
                 val exploreViewModel: ExploreViewModel = viewModel()
                 val status = profileViewModel.profileUiState
                 when (status) {
-                    is ProfileUiState.Loading -> {
-                        Blank()
-                    }
                     is ProfileUiState.Success -> {
-                        Profile(navController, status.data, status.data1, exploreViewModel, profileViewModel)
+                        Profile(navController, status.data, status.data1, profileViewModel, exploreViewModel)
                     }
                     is ProfileUiState.Error -> {
 
                     }
+
+                    else -> {}
                 }
             }
 
@@ -129,9 +123,6 @@ fun Routes() {
                 val status = homeViewModel.homeUIState
 
                 when (status) {
-                    is HomeUIState.Loading -> {
-                        Blank()
-                    }
                     is HomeUIState.Success -> {
                         Home(navController, homeViewModel = homeViewModel, user = status.user, dataStore = dataStore, exploreViewModel = exploreViewModel, listData = status.data)
                     }
@@ -153,10 +144,6 @@ fun Routes() {
                     navController.navigate(ListScreen.Profile.name)
 
                 }
-            }
-
-            composable(ListScreen.Blank.name) {
-                Blank()
             }
 
         }
